360eab68c790f55b385a27df0cd7f94c9ad0f8df
Donald Burr <dburr@borg-cube.com>
2015-06-18 16:23:41 -0700

    Try re-enabling de-duping, I may have misconfigured something

--- bootstrap.sh
+++ bootstrap.sh
@@ -110,22 +110,22 @@ sed -i.bak \
 # to stall... but maybe I'm not configuring it right... but let's disable
 # it for now
 #
-#sed -i.bak \
-#  -e '/ref.*bean=.*warcWriter/i \
-#\<bean class=\"org.archive.modules.recrawl.ContentDigestHistoryLoader\" \/\>' \
-#  -e '/ref.*bean=.*warcWriter/a \
-#\<bean class=\"org.archive.modules.recrawl.ContentDigestHistoryStorer\" \/\>' \
-#  -e '/CRAWL METADATA/i \
-#\<!-- optional, will use the main bdb module if omitted, just like old dedup --\>\
-#\<bean id=\"historyBdb\" class=\"org.archive.bdb.BdbModule\" autowire-candidate=\"false\"\>\
-#\<property name=\"dir\" value=\"history\" \/\>\
-#\<\/bean\>\
-#\<bean id=\"contentDigestHistory\" class=\"org.archive.modules.recrawl.BdbContentDigestHistory\"\>\
-#\<property name=\"bdbModule\"\>\
-#\<ref bean=\"historyBdb\" \/\>\
-#\<\/property\>\
-#\<\/bean\>' \
-#  /opt/heritrix-3.2.0/jobs/crawler/crawler-beans.cxml
+sed -i.bak \
+  -e '/ref.*bean=.*warcWriter/i \
+\<bean class=\"org.archive.modules.recrawl.ContentDigestHistoryLoader\" \/\>' \
+  -e '/ref.*bean=.*warcWriter/a \
+\<bean class=\"org.archive.modules.recrawl.ContentDigestHistoryStorer\" \/\>' \
+  -e '/CRAWL METADATA/i \
+\<!-- optional, will use the main bdb module if omitted, just like old dedup --\>\
+\<bean id=\"historyBdb\" class=\"org.archive.bdb.BdbModule\" autowire-candidate=\"false\"\>\
+\<property name=\"dir\" value=\"history\" \/\>\
+\<\/bean\>\
+\<bean id=\"contentDigestHistory\" class=\"org.archive.modules.recrawl.BdbContentDigestHistory\"\>\
+\<property name=\"bdbModule\"\>\
+\<ref bean=\"historyBdb\" \/\>\
+\<\/property\>\
+\<\/bean\>' \
+  /opt/heritrix-3.2.0/jobs/crawler/crawler-beans.cxml
 
 #cp /vagrant/crawler-beans.cxml.example /opt/heritrix-3.2.0/jobs/crawler/crawler-beans.cxml
 chown vagrant:vagrant /opt/heritrix-3.2.0/jobs/crawler/crawler-beans.cxml
