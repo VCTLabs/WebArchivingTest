fd927536d042d8eca892746d30c744acbb00e2e0
Donald Burr <dburr@borg-cube.com>
2015-06-27 17:02:37 -0700

    Improve python scripts

--- get_job_state.py
+++ get_job_state.py
@@ -4,14 +4,21 @@ import sys
 import time
 import hapy
 import requests
+import re
 requests.packages.urllib3.disable_warnings()
 
-def get_job_state(h, job_name):
-    print 'getting job state for job %s' % job_name,
+def get_job_state(h, job_name, verbose=False):
+    if verbose:
+        print 'getting job state for job %s' % job_name,
     info = h.get_job_info(job_name)
-    job_state = info['job']['availableActions']['value']
-    print '=> %s' % job_state
+    job_state = info['job']['statusDescription']
+    if verbose:
+        print '=> %s' % job_state
     return job_state
+
+def is_job_in_state(h, job_name, state):
+    current_state = get_job_state(h, job_name)
+    return re.search(state, current_state, re.IGNORECASE)
 
 def wait_for(h, job_name, func_name):
     print 'waiting for', func_name
@@ -20,10 +27,34 @@ def wait_for(h, job_name, func_name):
         time.sleep(1)
         info = h.get_job_info(job_name)
 
-name = sys.argv[1]
+def cmp(s1, s2):
+    return re.search(s2, s1, re.IGNORECASE)
+
+try:
+    name = sys.argv[1]
+except IndexError:
+    print "Usage: get_job_state.py job-name"
+    sys.exit(1)
+
 try:
     h = hapy.Hapy('https://localhost:8443', username='admin', password='password')
     state = get_job_state(h, name)
     print state
+    if cmp(state, "aborted") or cmp(state, "finished"):
+      print "job is aborted or finished"
+    elif cmp(state, "running"):
+      print "job is running"
+    elif cmp(state, "unbuilt"):
+      print "job is unbuilt"
+    elif cmp(state, "ready"):
+      print "job is ready"
+    elif cmp(state, "preparing"):
+      print "job is preparing"
+    elif cmp(state, "paused"):
+      print "job is paused"
+    elif cmp(state, "stopping"):
+      print "job is stopping"
+    else:
+      print "beats me!"
 except hapy.HapyException as he:
     print 'something went wrong:', he.message
--- run_job.py
+++ run_job.py
@@ -4,53 +4,75 @@ import sys
 import time
 import hapy
 import requests
+import re
 requests.packages.urllib3.disable_warnings()
 
-def get_job_state(h, job_name):
+def cmp(s1, s2):
+  return re.search(s2, s1, re.IGNORECASE)
+
+def get_state(h, job_name, verbose=False):
+  if verbose:
     print 'getting job state for job %s' % job_name,
-    info = h.get_job_info(job_name)
-    job_state = info['job']['availableActions']['value']
+  info = h.get_job_info(job_name)
+  job_state = info['job']['statusDescription']
+  if verbose:
     print '=> %s' % job_state
-    return job_state
+  return job_state
 
-def wait_for(h, job_name, func_name):
-    print 'waiting for', func_name
+def is_job_in_state(h, job_name, state):
+  current_state = get_job_state(h, job_name)
+  return cmp(current_state, state)
+
+def wait_for_state(h, job_name, state, verbose=False):
+  if verbose:
+    print 'waiting for', state
+  info = h.get_job_info(job_name)
+  while not cmp(info['job']['statusDescription'], state):
+    time.sleep(1)
     info = h.get_job_info(job_name)
-    while func_name not in info['job']['availableActions']['value']:
-        time.sleep(1)
-        info = h.get_job_info(job_name)
 
-name = sys.argv[1]
+def wait_for_available_action(h, job_name, action, verbose=False):
+  if verbose:
+    print 'waiting for', action
+  info = h.get_job_info(job_name)
+  while action not in info['job']['availableActions']['value']:
+    time.sleep(1)
+    info = h.get_job_info(job_name)
+
+# main
+
+try:
+    name = sys.argv[1]
+except IndexError:
+    print "Usage: run_job.py job-name"
+    sys.exit(1)
+
 try:
     h = hapy.Hapy('https://localhost:8443', username='admin', password='password')
-    state = get_job_state(h, name)
-    if state == "teardown":
-        print "job is ready to be re-run"
-        h.teardown_job(name)
-        print "waiting for build"
-        wait_for(h, name, 'build')
-        print "building job"
-        h.build_job(name)
-        print "waiting for launch"
-        wait_for(h, name, 'launch')
-        print "launching job"
-        h.launch_job(name)
-        print "waiting for pause"
-        wait_for(h, name, 'unpause')
-        print "unpausing job"
-        h.unpause_job(name)        
-    else:
-        print "waiting for build"
-        wait_for(h, name, 'build')
-        print "building job"
-        h.build_job(name)
-        print "waiting for launch"
-        wait_for(h, name, 'launch')
-        print "launching job"
-        h.launch_job(name)
-        print "waiting for pause"
-        wait_for(h, name, 'unpause')
-        print "unpausing job"
-        h.unpause_job(name)        
+    state = get_state(h, name)
+    if not(cmp(state, "aborted") or cmp(state, "finished")):
+      print "job is currently in state %s" % state
+      if cmp(state, "running"):
+        print "job is running, stopping it"
+        h.terminate_job(name)
+        wait_for_state(h, name, "aborted")
+
+    # job should be finished by now
+    print "tearing down job"
+    h.teardown_job(name)
+    wait_for_state(h, name, "unbuilt")
+
+    # now build it
+    h.build_job(name)
+    wait_for_state(h, name, "ready")
+    
+    # now start it
+    h.launch_job(name)
+    wait_for_state(h, name, "paused")
+    
+    # now run it
+    h.unpause_job(name)
+    wait_for_state(h, name, "running")
+
 except hapy.HapyException as he:
     print 'something went wrong:', he.message
